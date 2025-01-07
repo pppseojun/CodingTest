6817948112e0ef2baa7a4f6358438ffa6a5fedf0
pppseojun <135473205+pppseojun@users.noreply.github.com>
2025-01-07 21:10:18 +0900

    4,5번 문제 강의 진행

--- deepening/src/Main.java
+++ deepening/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.*;
+
+import javax.print.DocFlavor.STRING;
   
 // String - 1.문자 찾기(for문)
 // public class Main {
@@ -105,6 +107,63 @@ import java.util.*;
 //     }
 // }
 
+// String - 4. 단어 뒤집기기(StringBuilder 사용)
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n =sc.nextInt();
+//         String[] str = new String[n];
+//         for(int i=0; i<n;i++){
+//             str[i] = sc.next();
+//         }
+//         ArrayList<String> answer = new ArrayList<>();
+//         for(String x : str){
+//             String tmp = new StringBuilder(x).reverse().toString();
+//             answer.add(tmp);
+//         }
+//         for(String a : answer){
+//             System.out.println(a);
+//         }
+//     }
+// }
+
+// String - 4. 단어 뒤집기(lt, rt이용)
+// 1. String을 char배열로 만들어 준다
+// 2. lt, rt변수를 생성해서 각 문자를 담고 위치를 바꾸어준다
+// 2-1. 문자열이 홀수 일경우 가운대 중간에 위치한 문자를 기준으로 rt와 lt의 자리를 변경해준다
+// 2-2. 문자열이 짝수 일경우 각자 짝이 되는 위치의 문자 자리를 변경해준다.
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+//         String[] str = new String[n];
+//         for(int i=0; i<n; i++){
+//             str[i] = sc.next();
+//         }
+//         ArrayList<String> answer = new ArrayList<>();
+//         for(String x : str){
+//             char[] s = x.toCharArray();
+//             int lt = 0;
+//             int rt = x.length() - 1;
+//             while (lt<rt) {
+//                 char tmp = s[lt];
+//                 s[lt] = s[rt];
+//                 s[rt] = tmp;
+//                 lt++;
+//                 rt--;
+//             }
+//             String tmp = String.valueOf(s);
+               
+//             answer.add(tmp);
+//         }
+//         for(String a : answer){
+//             System.out.println(a);
+//         }
+//     }
+// }
+
+
+
 // String - 5. 특정문자 뒤집기
 // 문자열 입력받으면
 // 각 문자 배열로만들고
@@ -132,6 +191,35 @@ import java.util.*;
 //         System.out.println(new String(result));
 //     }
 // }
+
+// String - 5. 특정문자 뒤집기 (lt, rt 사용)
+// 1. 각 문자열 문자배열로 만들어주고
+// 2. 문자가 영어인지 특수 문자인지 구별
+// 3. rt lt 각각 이동하면서 두 위치의 값이 모두 영어이면 뒤집기 실행
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char[] c = str.toCharArray();
+        int lt = 0;
+        int rt = str.length() -1;
+        while(lt<rt){
+            if(!Character.isAlphabetic(c[lt])){
+                lt ++;
+            }else if(!Character.isAlphabetic(c[rt])){
+                rt--;
+            }else{
+                char tmp = c[lt];
+                c[lt] = c[rt];
+                c[rt] = tmp;
+                lt++;
+                rt--;
+            }
+        }
+        String tmp = String.valueOf(c);
+        System.out.println(tmp);
+    }
+}
 
 
 
